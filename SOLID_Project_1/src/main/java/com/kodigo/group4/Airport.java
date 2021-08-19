c6543e76b5ad848e32b64902a47dcea9a6a1b97a
Hector Montano <74574809+NamelessHGhoul95@users.noreply.github.com>
2021-08-19 13:32:32 -0600

    Michelle's part
    Michelle's part.

--- SOLID_Project_1/src/main/java/com/kodigo/group4/Airport.java
+++ SOLID_Project_1/src/main/java/com/kodigo/group4/Airport.java
@@ -4,8 +4,8 @@ public class Airport {
 
     public static void main(String[] args) {
 
-        EmailSender email = new EmailSender();
-        email.sendEmail("hectorjmon@hotmail.com","None");
+        //EmailSender email = new EmailSender();
+        //email.sendEmail("hectorjmon@hotmail.com","None");
         WeatherApp weather = new WeatherApp();
         System.out.println(weather.weatherInformationRetriever("Comalapa,sv"));
     }
