fdc65849b701fcf4f45ef0a11b7458fbead6be46
byronc377 <Byronc3601@hotmail.com>
2021-08-19 11:28:39 -0600

    new edit

--- SOLID_Project_1/src/main/java/com/kodigo/group4/FildeHandler.java
+++ SOLID_Project_1/src/main/java/com/kodigo/group4/FildeHandler.java
@@ -1,4 +1,59 @@
 package com.kodigo.group4;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Iterator;
 
 public class FildeHandler {
+
+
+    public void openFile() {
+        String fileName = "Data.xlsx";
+        String pathFile = "C:\\" + fileName;
+        Flight flight=new Flight();
+        try (FileInputStream file = new FileInputStream(new File(pathFile))) {
+
+            XSSFWorkbook book = new XSSFWorkbook(file);
+
+            XSSFSheet sheet = book.getSheetAt(0);
+
+            Iterator<Row> rowIterator = sheet.iterator();
+
+
+            StringBuilder sb = new StringBuilder();
+
+            Row row;
+
+            while (rowIterator.hasNext()) {
+                row = rowIterator.next();
+
+                Iterator<Cell> cellIterator = row.cellIterator();
+                Cell cell;
+
+                while (cellIterator.hasNext()) {
+
+                    cell = cellIterator.next();
+
+                    if (cell.getColumnIndex() == 0) {
+                        sb.append(cell.toString()).append("\t");
+                    } else {
+                        sb.append(String.format("%-30s", cell.toString())).append("\t");
+                    }
+
+                }
+
+                sb.append("\n");
+            }
+
+            System.out.println(sb.toString());
+        }   catch(IOException ex){
+            System.out.println("the file could not open "+ex);
+        }
+
+    }
 }
--- SOLID_Project_1/src/main/java/com/kodigo/group4/FlightAutomaticAdder.java
+++ SOLID_Project_1/src/main/java/com/kodigo/group4/FlightAutomaticAdder.java
@@ -1,4 +1,9 @@
 package com.kodigo.group4;
 
 public class FlightAutomaticAdder extends Flight{
+
+    public void importFile(){
+        FildeHandler f=new FildeHandler();
+        f.openFile();
+    }
 }
