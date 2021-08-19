f18384dd4a03546e042960961dc8845fd42ba679
EGuerreroM <72275181+EGuerreroM@users.noreply.github.com>
2021-08-19 11:11:03 -0600

    Airline and Aircraft Classes
    Added the airline and aircraft classes

--- SOLID_Project_1/src/main/java/com/kodigo/group4/Aircraft.java
+++ SOLID_Project_1/src/main/java/com/kodigo/group4/Aircraft.java
@@ -1,4 +1,26 @@
 package com.kodigo.group4;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter@Setter
 public class Aircraft extends Airline  {
+    private String model;
+    private int passengerCapacity;
+    private double range;
+    private int manufacturingYear;
+
+    public Aircraft(int code, String name, String model, int passengerCapacity, double range, int manufacturingYear) {
+        super(code, name);
+        this.model = model;
+        this.passengerCapacity = passengerCapacity;
+        this.range = range;
+        this.manufacturingYear = manufacturingYear;
+    }
+
+    public String showInformation(){
+
+        return "The Aircraft model is " + model + ", its passenger capacity is " + passengerCapacity + ", the range is " + range + " and the manufacturing year is " + manufacturingYear;
+
+    }
 }
--- SOLID_Project_1/src/main/java/com/kodigo/group4/Airline.java
+++ SOLID_Project_1/src/main/java/com/kodigo/group4/Airline.java
@@ -1,4 +1,13 @@
 package com.kodigo.group4;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Setter @Getter
 public class Airline {
+    private int code;
+    private String name;
 }
