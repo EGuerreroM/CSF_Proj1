caa0f062f6f9a15c419f1dab62277d6a7722f6eb
Hector Montano <74574809+NamelessHGhoul95@users.noreply.github.com>
2021-08-20 10:14:17 -0600

    Update Airport.java

--- SOLID_Project_1/src/main/java/com/kodigo/group4/Airport.java
+++ SOLID_Project_1/src/main/java/com/kodigo/group4/Airport.java
@@ -11,10 +11,6 @@ public class Airport {
 
     public static void main(String[] args) {
 
-        //EmailSender email = new EmailSender();
-        //email.sendEmail("hectorjmon@hotmail.com","None");
-        //WeatherApp weather = new WeatherApp();
-        //System.out.println(weather.weatherInformationRetriever("Comalapa,sv"));
         boolean flag = false;
         int selectedOption;
 
