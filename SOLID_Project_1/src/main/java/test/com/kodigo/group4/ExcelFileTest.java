6e9c83b77c4517463c81407bb4aee9dfad6a9890
byronc377 <Byronc3601@hotmail.com>
2021-09-09 20:17:08 -0600

    test for weather and excel

---
+++ SOLID_Project_1/src/main/java/test/com/kodigo/group4/ExcelFileTest.java
@@ -0,0 +1,45 @@
+package test.com.kodigo.group4; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* ExcelFile Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>sep. 9, 2021</pre> 
+* @version 1.0 
+*/ 
+public class ExcelFileTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: readFile(String filename) 
+* 
+*/ 
+@Test
+public void testReadFile() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: createFile(List<Flight> list) 
+* 
+*/ 
+@Test
+public void testCreateFile() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
