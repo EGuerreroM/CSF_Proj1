26d90fbe814f9d2e4d6c8adb9c0e114de36f7b03
byronc377 <Byronc3601@hotmail.com>
2021-09-09 18:29:31 -0600

    test for weather and excel

---
+++ SOLID_Project_1/src/main/java/com/kodigo/group4/ExcelFileTest.java
@@ -0,0 +1,46 @@
+package com.kodigo.group4;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+class ExcelFileTest {
+
+    private ExcelFile excelFileUnderTest;
+
+    @BeforeEach
+    void setUp() {
+        excelFileUnderTest = new ExcelFile();
+        excelFileUnderTest.weatherApp = mock(WeatherApp.class);
+        excelFileUnderTest.flight = mock(Flight.class);
+        excelFileUnderTest.separator = System.getProperty("file.separator");
+    }
+
+
+
+    @Test
+    void testCreateFile() {
+        // Setup
+        final Flight flight = new Flight();
+        flight.setFlightNumber("flightNumber");
+        flight.setStatus("status");
+        flight.setOriginCountry("originCountry");
+        flight.setOriginCity("originCity");
+        flight.setDepartureDate("departureDate");
+        flight.setDepartureTime("departureTime");
+        flight.setDestinationCountry("destinationCountry");
+        flight.setDestinationCity("destinationCity");
+        flight.setArrivalDate("arrivalDate");
+        flight.setArrivalTime("arrivalTime");
+        final List<Flight> list = List.of(flight);
+        when(excelFileUnderTest.weatherApp.getWeatherForecast()).thenReturn("result");
+
+        // Run the test
+        excelFileUnderTest.createFile(list);
+
+        // Verify the results
+    }
+}
---
+++ SOLID_Project_1/src/main/java/com/kodigo/group4/WeatherAppTest.java
@@ -0,0 +1,34 @@
+package com.kodigo.group4;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URLConnection;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class WeatherAppTest {
+
+    private WeatherApp weatherAppUnderTest;
+
+    @BeforeEach
+    void setUp() {
+        weatherAppUnderTest = new WeatherApp();
+    }
+
+    @Test
+    void testConnectToApi() {
+        // Setup
+
+        // Run the test
+        final URLConnection result = weatherAppUnderTest.connectToApi();
+
+        // Verify the results
+    }
+
+
+}
